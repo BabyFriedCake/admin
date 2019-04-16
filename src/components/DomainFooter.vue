8e4251beac46034fcdb5004f2a1839781d4e4480
FriedCake <33448924+BabyFriedCake@users.noreply.github.com>
2019-04-16 14:43:14 +0800

    Update DomainFooter.vue

--- src/components/DomainFooter.vue
+++ src/components/DomainFooter.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="footer_w">技术版权所有 © 天津卓朗科技发展有限公司 2013 TROILA.Inc. 保留一切权利。津ICP备10200312号-4</div>
+    <div class="footer_w">技术版权所有 ©  保留一切权利。津ICP备</div>
 </template>
 <style>
 @import '../assets/css/common.css';
